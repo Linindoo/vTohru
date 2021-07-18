23c66026db08a2ed77c8a30fc9bc6ddc19226120
lindo <1171935932@qq.com>
2021-07-18 12:48:58 +0800

    解决微服务参数设置的bug

--- src/main/java/cn/olange/vboot/microservice/MicroServiceClientIntroductionAdvice.java
+++ src/main/java/cn/olange/vboot/microservice/MicroServiceClientIntroductionAdvice.java
@@ -1,15 +1,12 @@
 package cn.olange.vboot.microservice;
 
 import cn.olange.vboot.context.VerticleApplicationContext;
-import io.micronaut.aop.InterceptedMethod;
 import io.micronaut.aop.InterceptorBean;
 import io.micronaut.aop.MethodInterceptor;
 import io.micronaut.aop.MethodInvocationContext;
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.core.type.Argument;
-import io.vertx.core.Future;
 import io.vertx.core.Handler;
-import io.vertx.core.Promise;
 import io.vertx.core.eventbus.DeliveryOptions;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
@@ -33,25 +30,27 @@ public class MicroServiceClientIntroductionAdvice implements MethodInterceptor<O
     @Override
     public Object intercept(MethodInvocationContext context) {
         if (context.hasAnnotation(Service.class)) {
-            InterceptedMethod interceptedMethod = InterceptedMethod.of(context);
-            logger.debug("process");
-            Promise<Object> promise = Promise.promise();
             Argument[] arguments = context.getExecutableMethod().getArguments();
             if (arguments.length == 0) {
                 throw new IllegalStateException("Invalid method" + context.getMethodName());
             }
             Map parameters = context.getParameterValueMap();
-            JsonObject _json = new JsonObject();
-            if (arguments.length > 1) {
-                for (int i = 0; i < arguments.length - 1; i++) {
-                    _json.put(arguments[i].getName(), parameters.get(arguments[i].getName()));
+            Object handlerObj = parameters.get(arguments[arguments.length - 1].getName());
+            if (handlerObj instanceof Handler) {
+                JsonObject _json = new JsonObject();
+                if (arguments.length > 1) {
+                    for (int i = 0; i < arguments.length - 1; i++) {
+                        _json.put(arguments[i].getName(), parameters.get(arguments[i].getName()));
+                    }
                 }
+                Handler handler = (Handler) handlerObj;
+                DeliveryOptions _deliveryOptions =  new DeliveryOptions();
+                _deliveryOptions.addHeader("action", context.getMethodName());
+                applicationContext.getVertx().eventBus().<String>request(context.getTarget().getClass().getName(), _json, _deliveryOptions, handler);
+                return null;
+            } else {
+                throw new IllegalStateException("method last params must be handler");
             }
-            Handler handler = (Handler) parameters.get(arguments[arguments.length - 1].getName());
-            DeliveryOptions _deliveryOptions =  new DeliveryOptions();
-            _deliveryOptions.addHeader("action", context.getMethodName());
-            applicationContext.getVertx().eventBus().<String>request(context.getTarget().getClass().getName(), _json, _deliveryOptions, handler);
-            return null;
         }
         return null;
     }
